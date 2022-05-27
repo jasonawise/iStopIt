d1a1b3a667fe62459f5e32de8ac4dbd053be30ef
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-26 20:03:06 -0400

    Initial Commit

---
+++ iStopIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iStopIt
+//
+//  Created by Jason Wise on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iStopIt/iStopItApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStopItApp.swift
+//  iStopIt
+//
+//  Created by Jason Wise on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct iStopItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
