ff93823f763304c5d0a0d70959339799f5a26640
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-30 18:59:33 -0400

    commit

---
+++ iStopIt/QuiterView.swift
@@ -0,0 +1,20 @@
+//
+//  QuiterView.swift
+//  iStopIt
+//
+//  Created by Jason Wise on 5/30/22.
+//
+
+import SwiftUI
+
+struct QuiterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuiterView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuiterView()
+    }
+}
